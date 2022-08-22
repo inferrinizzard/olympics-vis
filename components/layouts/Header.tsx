af068439b640240b52afd8bbfc7ad0ceb4147f19
inferrinizzard <inferrinizzard@gmail.com>
2022-08-22 13:28:19 -0700

    add basic breadcrumbs

--- components/layouts/Header.tsx
+++ components/layouts/Header.tsx
@@ -1,9 +1,30 @@
-import { Header as MantineHeader } from '@mantine/core';
+import { Anchor, Breadcrumbs, Header as MantineHeader } from '@mantine/core';
+import { useRouter } from 'next/router';
 
 const Header = () => {
+	const router = useRouter();
+	const path = router.asPath;
+
+	const pathBreadCrumbs = path
+		.split('/')
+		.slice(1)
+		.reduce(
+			(acc, slug) => ({
+				crumbs: [...acc.crumbs, { title: slug, href: acc.path + '/' + slug }],
+				path: acc.path + '/' + slug,
+			}),
+			{ crumbs: [] as { title: string; href: string }[], path: '' }
+		).crumbs;
+
 	return (
 		<MantineHeader height={60} p="xs">
-			{'Header'}
+			<Breadcrumbs>
+				{pathBreadCrumbs.map(({ title, href }) => (
+					<Anchor key={href} href={href}>
+						{title}
+					</Anchor>
+				))}
+			</Breadcrumbs>
 		</MantineHeader>
 	);
 };
