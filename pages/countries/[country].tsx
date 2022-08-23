7f9117f8ba80d1dc65c8ba48142e92169a9b7f6c
inferrinizzard <inferrinizzard@gmail.com>
2022-08-22 17:53:34 -0700

    add basic country page with prisma

--- pages/countries/[country].tsx
+++ pages/countries/[country].tsx
@@ -1,36 +1,52 @@
 import { type NextPage } from 'next';
 import { type GetStaticProps, type InferGetStaticPropsType, type GetStaticPaths } from 'next';
-import { useRouter } from 'next/router';
 
-import axios from 'axios';
-import { getRoute } from 'pages/api/_endpoint';
+import { Country, PrismaClient } from '@prisma/client';
 
-import { CountryDetail } from 'types/api';
+import { Box, Container, Grid, Title } from '@mantine/core';
+
+import { Calendar, Hash } from 'tabler-icons-react';
+
+import GridCell from 'components/grid/GridCell';
+import StatCard from 'components/grid/StatCard';
 
 export interface OlympicNOCProps {
-	country: CountryDetail;
+	country: Country;
 }
 
-export const getStaticProps: GetStaticProps<OlympicNOCProps> = ({ params }) =>
-	axios
-		.get(getRoute(['countries', params!.country as string]))
-		.then(res => ({ props: { country: res.data } }));
+export const getStaticProps: GetStaticProps<OlympicNOCProps> = async ({ params }) => {
+	const prisma = new PrismaClient();
 
-export const getStaticPaths: GetStaticPaths = () =>
-	axios.get(getRoute(['countries'])).then(({ data }) => ({
-		paths: data.map((country: string) => ({ params: { country } })),
-		fallback: false,
-	}));
+	const country = (await prisma.country.findFirst({
+		where: { country: params!.country as string },
+	}))!;
+
+	return { props: { country } };
+};
+
+export const getStaticPaths: GetStaticPaths = async () => {
+	const prisma = new PrismaClient();
+
+	const countries = await prisma.country.findMany({ select: { country: true } });
+
+	return { paths: countries.map(({ country }) => ({ params: { country } })), fallback: false };
+};
 
 const OlympicNOC: NextPage<InferGetStaticPropsType<typeof getStaticProps>> = ({ country }) => {
-	const { year, season } = useRouter().query;
-
 	return (
-		<>
-			<h1>{year}</h1>
-			<h1>{season}</h1>
-			<p>{JSON.stringify(country)}</p>
-		</>
+		<Container fluid>
+			<Grid>
+				<GridCell span={8}>
+					<Title order={1}>{`${country.name} (${country.country})`}</Title>
+					<Box sx={{ display: 'flex', columnGap: '1rem', flexShrink: 2, maxWidth: '100%' }}>
+						<StatCard icon={<Calendar />} title={'First Games'} text={''} />
+						<StatCard icon={<Hash />} title={'Total Medals'} text={''} />
+						<StatCard icon={<Hash />} title={'Games Hosted'} text={''} />
+					</Box>
+				</GridCell>
+				<GridCell span={4}></GridCell>
+			</Grid>
+		</Container>
 	);
 };
 
