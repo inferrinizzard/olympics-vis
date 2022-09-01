ee1f449269d38f7f4c38ce7f7f27cb118f9a2594
inferrinizzard <inferrinizzard@gmail.com>
2022-08-31 21:57:17 -0700

    convert countries main page to prisma

--- pages/countries/index.tsx
+++ pages/countries/index.tsx
@@ -2,23 +2,27 @@ import { type NextPage } from 'next';
 import { type GetStaticProps, type InferGetStaticPropsType } from 'next';
 import Link from 'next/link';
 
-import axios from 'axios';
-import { getRoute } from 'pages/api/_endpoint';
+import { Country, PrismaClient } from '@prisma/client';
 
 export interface CountriesProps {
-	countries: string[];
+	countries: Country[];
 }
 
-export const getStaticProps: GetStaticProps<CountriesProps> = () =>
-	axios.get(getRoute(['countries'])).then(res => ({ props: { countries: res.data } }));
+export const getStaticProps: GetStaticProps<CountriesProps> = async () => {
+	const prisma = new PrismaClient();
+
+	const countries = await prisma.country.findMany();
+
+	return { props: { countries } };
+};
 
 const Countries: NextPage<InferGetStaticPropsType<typeof getStaticProps>> = ({ countries }) => {
 	return (
 		<>
 			<div>Countries</div>
 			{countries.map(country => (
-				<div key={country}>
-					<Link href={`/countries/${country}`}>{country}</Link>
+				<div key={country.country}>
+					<Link href={`/countries/${country.country}`}>{country.country}</Link>
 				</div>
 			))}
 		</>
