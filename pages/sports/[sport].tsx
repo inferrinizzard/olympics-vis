f8e501687c79da11bdc73e7526ca940bc1cc4e5f
inferrinizzard <inferrinizzard@gmail.com>
2022-10-04 23:28:18 -0700

    fix title for events chart

--- pages/sports/[sport].tsx
+++ pages/sports/[sport].tsx
@@ -80,71 +80,70 @@ const OlympicSport: NextPage<InferGetStaticPropsType<typeof getStaticProps>> = (
 						</Box>
 					</GridCell>
 					<GridCell>
-						<Title order={2}>
-							<div style={{ width: '100%', height: '40vh' }}>
-								<ResponsiveLine
-									data={eventCountData}
-									margin={{ top: 50, right: 110, bottom: 50, left: 60 }}
-									xScale={{ type: 'point' }}
-									yScale={{
-										type: 'linear',
-										min: 0,
-										max: 'auto',
-										reverse: false,
-									}}
-									axisTop={null}
-									axisRight={null}
-									axisBottom={{
-										tickSize: 5,
-										tickPadding: 5,
-										tickRotation: 45,
-										legend: 'Olympic Games',
-										legendOffset: 36,
-										legendPosition: 'middle',
-									}}
-									axisLeft={{
-										tickSize: 5,
-										tickPadding: 5,
-										tickRotation: 0,
-										legend: 'Number of Events',
-										legendOffset: -40,
-										legendPosition: 'middle',
-									}}
-									pointSize={10}
-									pointColor={{ theme: 'background' }}
-									pointBorderWidth={2}
-									pointBorderColor={{ from: 'serieColor' }}
-									pointLabelYOffset={-12}
-									useMesh={true}
-									legends={[
-										{
-											anchor: 'bottom-right',
-											direction: 'column',
-											justify: false,
-											translateX: 100,
-											translateY: 0,
-											itemsSpacing: 0,
-											itemDirection: 'left-to-right',
-											itemWidth: 80,
-											itemHeight: 20,
-											itemOpacity: 0.75,
-											symbolSize: 12,
-											symbolShape: 'circle',
-											symbolBorderColor: 'rgba(0, 0, 0, .5)',
-											effects: [
-												{
-													on: 'hover',
-													style: {
-														itemBackground: 'rgba(0, 0, 0, .03)',
-														itemOpacity: 1,
-													},
+						<Title order={2}>{'Number of Events'}</Title>
+						<div style={{ width: '100%', height: '40vh' }}>
+							<ResponsiveLine
+								data={eventCountData}
+								margin={{ top: 50, right: 110, bottom: 50, left: 60 }}
+								xScale={{ type: 'point' }}
+								yScale={{
+									type: 'linear',
+									min: 0,
+									max: 'auto',
+									reverse: false,
+								}}
+								axisTop={null}
+								axisRight={null}
+								axisBottom={{
+									tickSize: 5,
+									tickPadding: 5,
+									tickRotation: 45,
+									legend: 'Olympic Games',
+									legendOffset: 36,
+									legendPosition: 'middle',
+								}}
+								axisLeft={{
+									tickSize: 5,
+									tickPadding: 5,
+									tickRotation: 0,
+									legend: 'Number of Events',
+									legendOffset: -40,
+									legendPosition: 'middle',
+								}}
+								pointSize={10}
+								pointColor={{ theme: 'background' }}
+								pointBorderWidth={2}
+								pointBorderColor={{ from: 'serieColor' }}
+								pointLabelYOffset={-12}
+								useMesh={true}
+								legends={[
+									{
+										anchor: 'bottom-right',
+										direction: 'column',
+										justify: false,
+										translateX: 100,
+										translateY: 0,
+										itemsSpacing: 0,
+										itemDirection: 'left-to-right',
+										itemWidth: 80,
+										itemHeight: 20,
+										itemOpacity: 0.75,
+										symbolSize: 12,
+										symbolShape: 'circle',
+										symbolBorderColor: 'rgba(0, 0, 0, .5)',
+										effects: [
+											{
+												on: 'hover',
+												style: {
+													itemBackground: 'rgba(0, 0, 0, .03)',
+													itemOpacity: 1,
 												},
-											],
-										},
-									]}
-								/>
-							</div>
-						</Title>
+											},
+										],
+									},
+								]}
+							/>
+						</div>
 					</GridCell>
 				</Grid.Col>
 			</Grid>
