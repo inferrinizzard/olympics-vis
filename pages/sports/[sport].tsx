6f1e2bb6ed01fa7622bc8ec06712bae3259714bf
inferrinizzard <inferrinizzard@gmail.com>
2022-10-04 21:57:42 -0700

    add basic line for sport events

--- pages/sports/[sport].tsx
+++ pages/sports/[sport].tsx
@@ -1,17 +1,20 @@
 import { type NextPage } from 'next';
 import type { GetStaticProps, InferGetStaticPropsType, GetStaticPaths } from 'next';
 
-import { PrismaClient, Sport } from '@prisma/client';
+import { Games, PrismaClient, Sport } from '@prisma/client';
 
 import { Box, Container, Grid, Image, Title } from '@mantine/core';
 
 import { Calendar, Hash, MapPin } from 'tabler-icons-react';
+
+import { ResponsiveLine } from '@nivo/line';
 
 import GridCell from 'components/grid/GridCell';
 import StatCard from 'components/grid/StatCard';
 
 export interface OlympicSportProps {
 	sport: Sport;
+	numEvents: Record<Games['game'], number>;
 }
 
 export const getStaticProps: GetStaticProps<OlympicSportProps> = async ({ params }) => {
@@ -19,7 +22,19 @@ export const getStaticProps: GetStaticProps<OlympicSportProps> = async ({ params
 
 	const sport = (await prisma.sport.findFirst({ where: { sport: params!.sport as string } }))!;
 
-	return { props: { sport } };
+	const countEvents = await prisma.sportsEvent.groupBy({
+		by: ['game'],
+		_count: { sport: true },
+		where: { sport: params!.sport as string },
+	});
+	const numEvents = countEvents
+		.reverse()
+		.reduce(
+			(acc, { game, _count: { sport: count } }) => (count ? { ...acc, [game]: count } : acc),
+			{}
+		);
+
+	return { props: { sport, numEvents } };
 };
 
 export const getStaticPaths: GetStaticPaths = async () => {
@@ -30,7 +45,17 @@ export const getStaticPaths: GetStaticPaths = async () => {
 	return { paths: sports.map(params => ({ params })), fallback: false };
 };
 
-const OlympicSport: NextPage<InferGetStaticPropsType<typeof getStaticProps>> = ({ sport }) => {
+const OlympicSport: NextPage<InferGetStaticPropsType<typeof getStaticProps>> = ({
+	sport,
+	numEvents,
+}) => {
+	const eventCountData = [
+		{
+			id: sport.sport,
+			data: Object.entries(numEvents).map(([game, count]) => ({ x: game, y: count })),
+		},
+	];
+
 	return (
 		<Container fluid sx={{ height: '100%' }}>
 			<Grid
@@ -53,6 +78,73 @@ const OlympicSport: NextPage<InferGetStaticPropsType<typeof getStaticProps>> = (
 							<StatCard icon={<Calendar />} title={'First Games'} text={'games'} />
 							<StatCard icon={<Hash />} title={'Number of Events'} text={100} />
 						</Box>
+					</GridCell>
+					<GridCell>
+						<Title order={2}>
+							<div style={{ width: '100%', height: '40vh' }}>
+								<ResponsiveLine
+									data={eventCountData}
+									margin={{ top: 50, right: 110, bottom: 50, left: 60 }}
+									xScale={{ type: 'point' }}
+									yScale={{
+										type: 'linear',
+										min: 0,
+										max: 'auto',
+										reverse: false,
+									}}
+									axisTop={null}
+									axisRight={null}
+									axisBottom={{
+										tickSize: 5,
+										tickPadding: 5,
+										tickRotation: 45,
+										legend: 'Olympic Games',
+										legendOffset: 36,
+										legendPosition: 'middle',
+									}}
+									axisLeft={{
+										tickSize: 5,
+										tickPadding: 5,
+										tickRotation: 0,
+										legend: 'Number of Events',
+										legendOffset: -40,
+										legendPosition: 'middle',
+									}}
+									pointSize={10}
+									pointColor={{ theme: 'background' }}
+									pointBorderWidth={2}
+									pointBorderColor={{ from: 'serieColor' }}
+									pointLabelYOffset={-12}
+									useMesh={true}
+									legends={[
+										{
+											anchor: 'bottom-right',
+											direction: 'column',
+											justify: false,
+											translateX: 100,
+											translateY: 0,
+											itemsSpacing: 0,
+											itemDirection: 'left-to-right',
+											itemWidth: 80,
+											itemHeight: 20,
+											itemOpacity: 0.75,
+											symbolSize: 12,
+											symbolShape: 'circle',
+											symbolBorderColor: 'rgba(0, 0, 0, .5)',
+											effects: [
+												{
+													on: 'hover',
+													style: {
+														itemBackground: 'rgba(0, 0, 0, .03)',
+														itemOpacity: 1,
+													},
+												},
+											],
+										},
+									]}
+								/>
+							</div>
+						</Title>
 					</GridCell>
 				</Grid.Col>
 			</Grid>
