52d13985b5ffb7a007be5a167a275de0710e6329
inferrinizzard <inferrinizzard@gmail.com>
2022-10-08 09:39:29 -0700

    add basic scrolling

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,5 @@
+import { useEffect, useRef } from 'react';
+
 import type { NextPage } from 'next';
 import type { GetStaticProps, InferGetStaticPropsType } from 'next';
 import Link from 'next/link';
@@ -29,28 +31,43 @@ const Hero: NextPage<InferGetStaticPropsType<typeof getStaticProps>> = ({
 	sports,
 	countries,
 }) => {
+	const gamesRef = useRef<HTMLDivElement>(null);
+
+	useEffect(() => {
+		const gamesInterval = setInterval(
+			() =>
+				gamesRef?.current?.scrollTo({
+					left: gamesRef?.current?.scrollLeft + 1,
+				}),
+			100
+		);
+		return () => clearInterval(gamesInterval);
+	}, []);
+
 	return (
 		<main>
 			<section>
 				<Title order={1}>{'Olympics Vis'}</Title>
 			</section>
-			<section style={{ overflowX: 'hidden' }}>
-				<div style={{ display: 'inline-flex' }}>
-					{games.map(game => (
-						<span key={game.game} style={{ width: '13rem', height: '13rem' }}>
-							<Link href={`/games/${game.game}`} passHref>
-								<Image
-									src={game.emblem}
-									alt={game.game}
-									fit={'contain'}
-									height={'100%'}
-									sx={{ cursor: 'pointer' }}
-									style={{ height: '100%' }}
-									styles={{ imageWrapper: { height: '100%' }, figure: { height: '100%' } }}
-								/>
-							</Link>
-						</span>
-					))}
+			<section>
+				<div ref={gamesRef} style={{ width: '100%', overflowX: 'scroll' }}>
+					<div style={{ display: 'inline-flex' }}>
+						{games.map(game => (
+							<span key={game.game} style={{ width: '13rem', height: '13rem' }}>
+								<Link href={`/games/${game.game}`} passHref>
+									<Image
+										src={game.emblem}
+										alt={game.game}
+										fit={'contain'}
+										height={'100%'}
+										sx={{ cursor: 'pointer' }}
+										style={{ height: '100%' }}
+										styles={{ imageWrapper: { height: '100%' }, figure: { height: '100%' } }}
+									/>
+								</Link>
+							</span>
+						))}
+					</div>
 				</div>
 				<Link href="/games">{'See all →'}</Link>
 			</section>
