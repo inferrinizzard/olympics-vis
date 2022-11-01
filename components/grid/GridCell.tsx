2c35828140e183efbeaa451140028d449a5bbfa7
inferrinizzard <inferrinizzard@gmail.com>
2022-10-31 18:02:31 -0700

    add variants to gridcell

--- components/grid/GridCell.tsx
+++ components/grid/GridCell.tsx
@@ -1,18 +1,31 @@
-import { Paper, type PaperProps, type Sx } from '@mantine/core';
+import { Paper, type ButtonProps, type PaperProps, type Sx } from '@mantine/core';
 
 interface GridCellProps {
 	colour: string;
 }
 
-const GridCell: React.FC<GridCellProps & PaperProps> = ({ children, colour, sx, ...props }) => {
+const GridCell: React.FC<GridCellProps & PaperProps & Pick<ButtonProps, 'variant'>> = ({
+	children,
+	variant = 'filled',
+	colour,
+	sx,
+	...props
+}) => {
 	return (
 		<Paper
 			p="sm"
 			sx={theme => ({
-				...(sx as Sx),
 				accentColor: 'white',
-				backgroundColor: theme.colors[colour][5],
 				borderRadius: '1rem',
+				...(variant === 'outline'
+					? {
+							border: '0.25rem solid',
+							borderColor: theme.colors[colour][5],
+					  }
+					: {
+							backgroundColor: colour in theme.colors ? theme.colors[colour][5] : colour,
+					  }),
+				...(sx as Sx),
 			})}
 			{...props}>
 			{children}
