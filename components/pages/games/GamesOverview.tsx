bd3f25070fe87efbd7c72e911c1cb10972628554
inferrinizzard <inferrinizzard@gmail.com>
2022-11-02 08:32:39 -0700

    break out games overview component

---
+++ components/pages/games/GamesOverview.tsx
@@ -0,0 +1,44 @@
+import { type Games } from '@prisma/client';
+
+import { Title, Box, Text, Image } from '@mantine/core';
+
+import { BuildingSkyscraper, Calendar, CalendarEvent, Run, Hash } from 'tabler-icons-react';
+
+import GridCell from 'components/grid/GridCell';
+import StatCard from 'components/grid/StatCard';
+
+interface GamesOverviewProps {
+	game: Games;
+}
+
+const GamesOverview: React.FC<GamesOverviewProps> = ({ game }) => {
+	return (
+		<GridCell
+			colour="green"
+			sx={theme => ({
+				display: 'flex',
+				justifyContent: 'space-between',
+				backgroundColor: theme.colors.green[1],
+			})}>
+			<Box m="xs" sx={{ width: '75%', display: 'flex', flexDirection: 'column' }}>
+				<Title order={1}>{`${game.year} ${
+					game.season[0].toUpperCase() + game.season.slice(1)
+				} Olympics`}</Title>
+				<Title order={3}>{game.title}</Title>
+				<Text sx={{ flexGrow: 1 }}>Description goes here</Text>
+				<Box sx={{ display: 'flex', columnGap: '1rem', flexShrink: 2, maxWidth: '100%' }}>
+					<StatCard Icon={BuildingSkyscraper} title={'Host'} text={game.host} />
+					<StatCard Icon={Calendar} title={'Start Date'} text={game.start_date} />
+					<StatCard Icon={CalendarEvent} title={'End Date'} text={game.end_date} />
+					<StatCard Icon={Run} title={'Total Athletes'} text={game.num_athletes} />
+					<StatCard Icon={Hash} title={'Total Countries'} text={100} />
+				</Box>
+			</Box>
+			<Box p="sm" sx={{ width: '25%' }}>
+				<Image src={game.emblem} alt={'Olympic emblem for ' + game} sx={{ width: '100%' }} />
+			</Box>
+		</GridCell>
+	);
+};
+
+export default GamesOverview;
--- pages/games/[game].tsx
+++ pages/games/[game].tsx
@@ -14,12 +14,11 @@ import worldCountries from 'resources/countries.min.geo.json';
 import nocIsoLookup from 'resources/geo_noc_map.json';
 import { ResponsiveChoropleth } from '@nivo/geo';
 
-import { Box, Container, Grid, Image, Title, Text, Table } from '@mantine/core';
+import { Container, Grid, Image, Title, Table } from '@mantine/core';
 
-import { BuildingSkyscraper, Calendar, CalendarEvent, Hash, Run } from 'tabler-icons-react';
+import GamesOverview from 'components/pages/games/GamesOverview';
 
 import GridCell from 'components/grid/GridCell';
-import StatCard from 'components/grid/StatCard';
 import BackButton from 'components/layouts/BackButton';
 
 export interface OlympicGameSeasonProps {
@@ -88,25 +87,7 @@ const OlympicGameSeason: NextPage<InferGetStaticPropsType<typeof getStaticProps>
 					borderRadius: '1rem',
 				})}>
 				<Grid.Col span={8}>
-					<GridCell colour="green" sx={{ display: 'flex', justifyContent: 'space-between' }}>
-						<Box sx={{ width: '75%', display: 'flex', flexDirection: 'column' }} m="xs">
-							<Title order={1}>{`${game.year} ${
-								game.season[0].toUpperCase() + game.season.slice(1)
-							} Olympics`}</Title>
-							<Title order={3}>{game.title}</Title>
-							<Text sx={{ flexGrow: 1 }}>Description goes here</Text>
-							<Box sx={{ display: 'flex', columnGap: '1rem', flexShrink: 2, maxWidth: '100%' }}>
-								<StatCard Icon={BuildingSkyscraper} title={'Host'} text={game.host} />
-								<StatCard Icon={Calendar} title={'Start Date'} text={game.start_date} />
-								<StatCard Icon={CalendarEvent} title={'End Date'} text={game.end_date} />
-								<StatCard Icon={Run} title={'Total Athletes'} text={game.num_athletes} />
-								<StatCard Icon={Hash} title={'Total Countries'} text={100} />
-							</Box>
-						</Box>
-						<Box p="sm" sx={{ width: '25%' }}>
-							<Image src={game.emblem} alt={'Olympic emblem for ' + game} sx={{ width: '100%' }} />
-						</Box>
-					</GridCell>
+					<GamesOverview game={game} />
 				</Grid.Col>
 				<Grid.Col span={4}>
 					<GridCell colour="green">
