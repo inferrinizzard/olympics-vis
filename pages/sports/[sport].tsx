fb8b6c2fba60adcf8d0c9c111bedbc4b7a96abb5
inferrinizzard <inferrinizzard@gmail.com>
2022-11-03 08:51:41 -0700

    break out sport events chart

--- pages/sports/[sport].tsx
+++ pages/sports/[sport].tsx
@@ -6,11 +6,11 @@ import { PrismaClient, type CountrySportsMedals, type Games, type Sport } from '
 import { Container, Grid, Title } from '@mantine/core';
 
 import { ResponsiveBar } from '@nivo/bar';
-import { ResponsiveLine } from '@nivo/line';
 
 import SportsOverview from 'components/pages/sports/SportsOverview';
 import GridCell from 'components/grid/GridCell';
 import { sortByMedals } from 'pages/utils';
+import SportsEventsChart from 'components/pages/sports/SportsEventsChart';
 
 export interface OlympicSportProps {
 	sport: Sport;
@@ -56,13 +56,6 @@ const OlympicSport: NextPage<InferGetStaticPropsType<typeof getStaticProps>> = (
 	numEvents,
 	countrySportsMedals,
 }) => {
-	const eventCountData = [
-		{
-			id: sport.sport,
-			data: Object.entries(numEvents).map(([game, count]) => ({ x: game, y: count })),
-		},
-	];
-
 	const leadingCountries = countrySportsMedals.sort(sortByMedals).reverse().slice(0, 10);
 
 	return (
@@ -78,72 +71,7 @@ const OlympicSport: NextPage<InferGetStaticPropsType<typeof getStaticProps>> = (
 					<SportsOverview sport={sport} />
 				</Grid.Col>
 				<Grid.Col p={0}>
-					<GridCell colour="red">
-						<Title order={2}>{'Number of Events'}</Title>
-						<div style={{ width: '100%', height: '40vh' }}>
-							<ResponsiveLine
-								data={eventCountData}
-								margin={{ top: 50, right: 110, bottom: 50, left: 60 }}
-								xScale={{ type: 'point' }}
-								yScale={{
-									type: 'linear',
-									min: 0,
-									max: 'auto',
-									reverse: false,
-								}}
-								axisTop={null}
-								axisRight={null}
-								axisBottom={{
-									tickSize: 5,
-									tickPadding: 5,
-									tickRotation: 45,
-									legend: 'Olympic Games',
-									legendOffset: 36,
-									legendPosition: 'middle',
-								}}
-								axisLeft={{
-									tickSize: 5,
-									tickPadding: 5,
-									tickRotation: 0,
-									legend: 'Number of Events',
-									legendOffset: -40,
-									legendPosition: 'middle',
-								}}
-								pointSize={10}
-								pointColor={{ theme: 'background' }}
-								pointBorderWidth={2}
-								pointBorderColor={{ from: 'serieColor' }}
-								pointLabelYOffset={-12}
-								useMesh={true}
-								legends={[
-									{
-										anchor: 'bottom-right',
-										direction: 'column',
-										justify: false,
-										translateX: 100,
-										translateY: 0,
-										itemsSpacing: 0,
-										itemDirection: 'left-to-right',
-										itemWidth: 80,
-										itemHeight: 20,
-										itemOpacity: 0.75,
-										symbolSize: 12,
-										symbolShape: 'circle',
-										symbolBorderColor: 'rgba(0, 0, 0, .5)',
-										effects: [
-											{
-												on: 'hover',
-												style: {
-													itemBackground: 'rgba(0, 0, 0, .03)',
-													itemOpacity: 1,
-												},
-											},
-										],
-									},
-								]}
-							/>
-						</div>
-					</GridCell>
+					<SportsEventsChart sport={sport} numEvents={numEvents} />
 					<GridCell colour="red">
 						<Title order={2}>{'Leading Countries'}</Title>
 						<div style={{ width: '100%', height: '40vh' }}>
