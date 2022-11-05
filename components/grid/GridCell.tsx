adfbaa24c90e675eed2d0195654afdf86085a774
inferrinizzard <inferrinizzard@gmail.com>
2022-11-04 18:30:54 -0700

    remove variant from grid cell component

--- components/grid/GridCell.tsx
+++ components/grid/GridCell.tsx
@@ -1,17 +1,11 @@
-import { Paper, type ButtonProps, type PaperProps, type Sx } from '@mantine/core';
+import { Paper, type PaperProps, type Sx } from '@mantine/core';
 
 interface GridCellProps extends PaperProps {
 	colour: string;
 	sx?: Sx;
 }
 
-const GridCell: React.FC<GridCellProps & Pick<ButtonProps, 'variant'>> = ({
-	children,
-	variant = 'filled',
-	colour,
-	sx,
-	...props
-}) => {
+const GridCell: React.FC<GridCellProps> = ({ children, colour, sx, ...props }) => {
 	return (
 		<Paper
 			p="sm"
@@ -19,14 +13,7 @@ const GridCell: React.FC<GridCellProps & Pick<ButtonProps, 'variant'>> = ({
 			sx={theme => ({
 				accentColor: 'white',
 				borderRadius: '1rem',
-				...(variant === 'outline'
-					? {
-							border: '0.25rem solid',
-							borderColor: theme.colors[colour][5],
-					  }
-					: {
-							backgroundColor: colour in theme.colors ? theme.colors[colour][5] : colour,
-					  }),
+				backgroundColor: colour in theme.colors ? theme.colors[colour][5] : colour,
 				...(sx instanceof Function ? sx(theme) : sx),
 			})}
 			{...props}>
