4a9b9a1e732d4e2ed925fce0e3e84cd5afb22eaa
inferrinizzard <inferrinizzard@gmail.com>
2022-11-06 18:18:57 -0800

    use home icon for hosted games

--- components/pages/countries/CountryOverview.tsx
+++ components/pages/countries/CountryOverview.tsx
@@ -1,7 +1,7 @@
 import { type Country, type Games, type Sport } from '@prisma/client';
 
 import { Box, Image, Title } from '@mantine/core';
-import { Calendar, Hash, Run } from 'tabler-icons-react';
+import { Calendar, Hash, Home, Run } from 'tabler-icons-react';
 
 import GridCell from 'components/grid/GridCell';
 import StatCard from 'components/grid/StatCard';
@@ -36,7 +36,7 @@ const CountryOverview: React.FC<CountryOverviewProps> = ({
 			<Box p="xs" sx={{ display: 'flex', rowGap: '1rem', flexDirection: 'column' }}>
 				<StatCard Icon={Calendar} title={'First Games'} text={firstGames} />
 				<StatCard Icon={Hash} title={'Total Medals'} text={`${totalMedals}`} />
-				<StatCard Icon={Hash} title={'Games Hosted'} text={''} />
+				<StatCard Icon={Home} title={'Games Hosted'} text={''} />
 				{bestGames && <StatCard Icon={Calendar} title={'Best Games'} text={bestGames} />}
 				{bestSport && <StatCard Icon={Run} title={'Best Sport'} text={bestSport} />}
 			</Box>
