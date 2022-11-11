7c8d9457226baf8248b02b22b223473ba6bda010
inferrinizzard <inferrinizzard@gmail.com>
2022-11-10 20:07:12 -0800

    add excerpt todo for sports

--- components/pages/sports/SportsOverview.tsx
+++ components/pages/sports/SportsOverview.tsx
@@ -1,3 +1,5 @@
+import { useState, useEffect } from 'react';
+
 import { Box, Image, Title } from '@mantine/core';
 
 import { MapPin, Calendar, Hash } from 'tabler-icons-react';
@@ -5,12 +7,19 @@ import { MapPin, Calendar, Hash } from 'tabler-icons-react';
 import { type OlympicSportProps } from 'pages/sports/[sport]';
 import GridCell from 'components/grid/GridCell';
 import StatCard from 'components/grid/StatCard';
+import { getWikipediaExcerpt, getWikipediaUrl } from 'src/utils/wikipedia';
 
 interface SportsOverviewProps {
 	sport: OlympicSportProps['sport'];
 }
 
 const SportsOverview: React.FC<SportsOverviewProps> = ({ sport }) => {
+	const [description, setDescription] = useState('Excerpt TODO');
+
+	useEffect(() => {
+		// getWikipediaExcerpt(getWikipediaUrl('sports', sport.name)).then(setDescription);
+	}, [sport]);
+
 	return (
 		<GridCell bg="red">
 			<Box sx={{ display: 'flex' }}>
@@ -18,7 +27,7 @@ const SportsOverview: React.FC<SportsOverviewProps> = ({ sport }) => {
 					<Title order={2}>{`${sport.name} (${sport.sport})`}</Title>
 					<Image src={sport.icon} width={100} alt={sport.sport + ' sport icon'} />
 				</Box>
-				<Box sx={{ flexGrow: 1, padding: '1rem' }}>Lorem Ipsum</Box>
+				<Box sx={{ flexGrow: 1, padding: '1rem' }}>{description}</Box>
 				<Box
 					sx={{ display: 'flex', rowGap: '1rem', flexDirection: 'column', alignSelf: 'flex-end' }}>
 					<StatCard Icon={MapPin} title={'Best Country'} text={'Country'} />
