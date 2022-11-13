6637d9641cb2d65b50ae585062c542a799fc4d97
inferrinizzard <inferrinizzard@gmail.com>
2022-11-13 11:53:47 -0800

    add virtualization for card scroller

--- components/pages/hero/CardScroller.tsx
+++ components/pages/hero/CardScroller.tsx
@@ -2,6 +2,7 @@ import { useEffect, useRef } from 'react';
 import Link from 'next/link';
 
 import { Box, Tooltip } from '@mantine/core';
+import { useVirtualizer } from '@tanstack/react-virtual';
 
 import CardLink from 'components/layouts/CardLink';
 import { autoscroll } from 'src/utils/autoScroll';
@@ -24,6 +25,15 @@ const CardScroller = <T extends Record<string, string | number>>({
 	color,
 }: CardScrollerProps<T>) => {
 	const scrollRef = useRef<HTMLDivElement>(null);
+	const virtualizeWrapperRef = useRef<HTMLDivElement>(null);
+
+	const virtualizer = useVirtualizer({
+		horizontal: true,
+		count: data.length,
+		getScrollElement: () => virtualizeWrapperRef.current,
+		estimateSize: () => 16 * 13.5, // ref: ITEM, 16px per rem
+		overscan: 3,
+	});
 
 	useEffect(() => {
 		const scrollInterval = autoscroll(scrollRef, direction * (1 + Math.random()));
@@ -39,19 +49,28 @@ const CardScroller = <T extends Record<string, string | number>>({
 				className="disable-scrollbar"
 				w="100%"
 				sx={{ overflowX: 'scroll' }}>
-				<Box sx={{ display: 'inline-flex', flexDirection: direction > 0 ? 'row' : 'row-reverse' }}>
-					{data.map(datum => (
-						<Tooltip key={datum[idKey]} label={datum[idKey]} position="bottom">
-							<Box m="0.25rem" h="13rem" w="13rem" sx={{ display: 'inline-block' }}>
-								<CardLink
-									href={`/${route}/${datum[idKey]}`}
-									img={datum[imageKey] as string}
-									alt={datum[idKey] as string}
-									hoverColour={color}
-								/>
-							</Box>
-						</Tooltip>
-					))}
+				<Box
+					ref={virtualizeWrapperRef}
+					w={virtualizer.getTotalSize()}
+					sx={{ display: 'inline-flex', flexDirection: direction > 0 ? 'row' : 'row-reverse' }}>
+					{virtualizer.getVirtualItems().map(item => {
+						const datum = data[item.index];
+						return (
+							<Tooltip key={datum[idKey]} label={datum[idKey]} position="bottom">
+								<Box // ITEM: width + left & right margin
+									m="0.25rem"
+									w="13rem"
+									h="13rem">
+									<CardLink
+										href={`/${route}/${datum[idKey]}`}
+										img={datum[imageKey] as string}
+										alt={datum[idKey] as string}
+										hoverColour={color}
+									/>
+								</Box>
+							</Tooltip>
+						);
+					})}
 				</Box>
 			</Box>
 			<Link href={`/${route}`}>{'See all →'}</Link>
