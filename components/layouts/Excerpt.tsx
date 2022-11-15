606aef6d882ba8017f028603e190e48231862132
inferrinizzard <inferrinizzard@gmail.com>
2022-11-14 18:30:09 -0800

    remove loader from excerpt component

--- components/layouts/Excerpt.tsx
+++ components/layouts/Excerpt.tsx
@@ -1,19 +1,15 @@
 import { Spoiler, Text } from '@mantine/core';
 
-import TextLoader from './TextLoader';
-
 interface ExcerptProps {
 	text: string;
 	height?: number;
 }
 
 const Excerpt: React.FC<ExcerptProps> = ({ text, height }) => {
-	return text ? (
+	return (
 		<Spoiler maxHeight={height ?? 250} showLabel="Keep Reading" hideLabel="Hide">
 			<Text sx={{ color: 'white' }}>{text}</Text>
 		</Spoiler>
-	) : (
-		<TextLoader width="100%" />
 	);
 };
 
