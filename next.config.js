2789bedeae232f330398b37dc908370ae0dfa328
inferrinizzard <inferrinizzard@gmail.com>
2024-08-14 00:11:34 -0700

    bootstrap app dir + mantine template

--- next.config.js
+++ next.config.js
@@ -1,16 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	reactStrictMode: true,
+	swcMinify: true,
+	experimental: {
+		optimizePackageImports: ["@mantine/core", "@mantine/hooks"],
+	},
 	images: {
-		domains: ['upload.wikimedia.org'],
+		domains: ["upload.wikimedia.org"],
 		dangerouslyAllowSVG: true,
 		contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
 		minimumCacheTTL: 600,
 	},
 };
 
-const withBundleAnalyzer = require('@next/bundle-analyzer')({
-	enabled: process.env.ANALYZE === 'true',
+const withBundleAnalyzer = require("@next/bundle-analyzer")({
+	enabled: process.env.ANALYZE === "true",
 });
 
 module.exports = withBundleAnalyzer(nextConfig);
