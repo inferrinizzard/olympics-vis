4827f8a63a343a0b6a0ea92662b74e7ffdc4616c
inferrinizzard <inferrinizzard@gmail.com>
2024-08-15 08:01:23 -0700

    temp sports all page

--- app/sports/page.tsx
+++ app/sports/page.tsx
@@ -1,5 +1,20 @@
-export const SportsAll = () => {
-	return <div>{"Sports"}</div>;
+import { Title } from "@mantine/core";
+
+import { getAllSports, getSportWithSeason } from "lib/db";
+
+export const SportsAll = async () => {
+	const sports = await getAllSports();
+
+	// const sportsList = await getSportWithSeason();
+
+	return (
+		<>
+			<Title order={1}>{"Sports"}</Title>
+			{sports.map(({ sport, name }) => (
+				<div key={sport}>{sport}</div>
+			))}
+		</>
+	);
 };
 
 export default SportsAll;
