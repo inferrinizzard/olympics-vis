7ac4cce0bd6ee5b51d554d7f663d9b8ef1094bc7
inferrinizzard <inferrinizzard@gmail.com>
2024-08-15 07:50:26 -0700

    :recycle:

--- lib/db/prisma.ts
+++ lib/db/prisma.ts
@@ -1,13 +1,13 @@
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient } from "@prisma/client";
 
 declare global {
-  // allow global `var` declarations
-  var prisma: PrismaClient;
+	// allow global `var` declarations
+	var prisma: PrismaClient;
 }
 
 // biome-ignore lint/suspicious/noRedeclare: <explanation>
-export const prisma = global.prisma || new PrismaClient({ log: ['query'] });
+export const prisma = global.prisma || new PrismaClient({ log: ["query"] });
 
-if (process.env.NODE_ENV !== 'production') global.prisma = prisma;
+if (process.env.NODE_ENV !== "production") global.prisma = prisma;
 
 export default prisma;
