3fb5fc0f2fc21f7216f4a1bb5a785cd252160282
inferrinizzard <inferrinizzard@gmail.com>
2024-08-25 22:37:15 -0700

    remove remaining sx props

--- components/controls/ColorSchemeToggle.tsx
+++ components/controls/ColorSchemeToggle.tsx
@@ -1,7 +1,7 @@
-import { ActionIcon, Group, useMantineColorScheme } from '@mantine/core';
+import { ActionIcon, Group, useMantineColorScheme } from "@mantine/core";
 
-import Sun from 'tabler-icons-react/dist/icons/sun';
-import Moon from 'tabler-icons-react/dist/icons/moon';
+import Sun from "tabler-icons-react/dist/icons/sun";
+import Moon from "tabler-icons-react/dist/icons/moon";
 
 const ColorSchemeToggle = () => {
 	const { colorScheme, toggleColorScheme } = useMantineColorScheme();
@@ -10,12 +10,17 @@ const ColorSchemeToggle = () => {
 		<ActionIcon
 			size="lg"
 			variant="filled"
-			sx={theme => ({
-				backgroundColor: theme.colorScheme === 'dark' ? theme.colors.dark[6] : theme.colors.gray[0],
-				color: theme.colorScheme === 'dark' ? theme.colors.yellow[4] : theme.colors.blue[6],
-			})}
-			onClick={() => toggleColorScheme()}>
-			{colorScheme === 'dark' ? <Sun width={20} height={20} /> : <Moon width={20} height={20} />}
+			// sx={theme => ({
+			// 	backgroundColor: theme.colorScheme === 'dark' ? theme.colors.dark[6] : theme.colors.gray[0],
+			// 	color: theme.colorScheme === 'dark' ? theme.colors.yellow[4] : theme.colors.blue[6],
+			// })}
+			onClick={() => toggleColorScheme()}
+		>
+			{colorScheme === "dark" ? (
+				<Sun width={20} height={20} />
+			) : (
+				<Moon width={20} height={20} />
+			)}
 		</ActionIcon>
 	);
 };
--- components/layouts/Excerpt.tsx
+++ components/layouts/Excerpt.tsx
@@ -1,4 +1,4 @@
-import { Spoiler, Text } from '@mantine/core';
+import { Spoiler, Text } from "@mantine/core";
 
 interface ExcerptProps {
 	text: string;
@@ -7,8 +7,12 @@ interface ExcerptProps {
 
 const Excerpt: React.FC<ExcerptProps> = ({ text, height }) => {
 	return (
-		<Spoiler maxHeight={height ?? 250} showLabel="Keep Reading" hideLabel="Hide">
-			<Text sx={{ color: 'white' }}>{text}</Text>
+		<Spoiler
+			maxHeight={height ?? 250}
+			showLabel="Keep Reading"
+			hideLabel="Hide"
+		>
+			<Text style={{ color: "white" }}>{text}</Text>
 		</Spoiler>
 	);
 };
