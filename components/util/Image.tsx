b938544edb75ed41860f911840fb8c56e5a0929d
inferrinizzard <inferrinizzard@gmail.com>
2024-09-04 21:40:17 -0700

    basic custom image component

--- components/util/Image.tsx
+++ components/util/Image.tsx
@@ -1,18 +1,63 @@
 import { useState } from "react";
 import NextImage, { type ImageProps as NextImageProps } from "next/image";
 
-interface ImageProps extends NextImageProps {
+import type { CountryKey, GamesKey, SportKey } from "types/prisma";
+
+import sharedFlags from "public/images/country/shared/sharedFlags.json";
+
+type CountryImageProps = { dir: "country"; code: CountryKey };
+type GamesImageProps = { dir: "games"; code: GamesKey };
+type SportsImageProps = { dir: "sports"; code: SportKey; games?: GamesKey };
+
+interface FallbackImageProps extends Omit<NextImageProps, "src"> {
 	fallback?: string;
 }
 
-export const Image = ({ fallback, src, ...props }: ImageProps) => {
-	const [imgSrc, setImgSrc] = useState(src);
+type ImageProps = (CountryImageProps | GamesImageProps | SportsImageProps) &
+	FallbackImageProps;
+
+const useCountryImageSrc = (code: CountryKey) => {
+	if (code in sharedFlags) {
+		const sharedFlag = sharedFlags[code as keyof typeof sharedFlags];
+		return `/images/country/shared/${sharedFlag}`;
+	}
+	return `/images/country/${code}.svg`; // TODO: handle avif
+};
+
+const useGamesImageSrc = (code: GamesKey) => {
+	return `/images/games/${code}/emblem.svg`; // TODO: handle avif
+};
+
+const useSportsImageSrc = (code: SportKey, games?: GamesKey) => {
+	if (games) {
+		return `/images/games/${games}/sports/${code}.avif`; // TODO: handle fallback
+	}
+
+	if (true) {
+		return `/images/sports/official/${code}.svg`;
+	}
+
+	return `/images/sports/${code}.svg`;
+};
+
+export const Image = ({ dir, code, fallback, ...props }: ImageProps) => {
+	const src =
+		dir === "country"
+			? useCountryImageSrc(code)
+			: dir === "games"
+				? useGamesImageSrc(code)
+				: dir === "sports"
+					? useSportsImageSrc(
+							code,
+							(props as Omit<SportsImageProps, "dir" | "code">).games,
+						)
+					: "";
 
 	return (
 		<NextImage
 			{...props}
-			src={imgSrc}
-			onError={() => setImgSrc(fallback ?? src)}
+			src={src}
+			// onError={() => setImgSrc(fallback ?? src)}
 		/>
 	);
 };
