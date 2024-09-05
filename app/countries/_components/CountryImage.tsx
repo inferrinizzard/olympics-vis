90cbea0df0423b46f1ddbf2c74823e6bebf01ff1
inferrinizzard <inferrinizzard@gmail.com>
2024-09-05 08:11:00 -0700

    temp images

--- app/countries/_components/CountryImage.tsx
+++ app/countries/_components/CountryImage.tsx
@@ -1,4 +1,5 @@
-import Image, { type ImageProps } from "next/image";
+import { existsSync, readdirSync, readFileSync } from "fs";
+import Image, { getImageProps, type ImageProps } from "next/image";
 
 import sharedFlags from "public/images/country/shared/sharedFlags.json";
 
@@ -9,7 +10,40 @@ export interface CountryImageProps extends ImageProps {
 export const CountryImage = ({ code, ...props }: CountryImageProps) => {
 	if (code in sharedFlags) {
 		const sharedFlag = sharedFlags[code as keyof typeof sharedFlags];
-		return <Image {...props} src={`/images/country/shared/${sharedFlag}`} />;
+		return (
+			<Image
+				{...props}
+				src={`/images/country/shared/${sharedFlag}`}
+				unoptimized={sharedFlag.endsWith(".svg")}
+			/>
+		);
 	}
-	return <Image {...props} />;
+
+	if (existsSync(`public/images/country/${code}.svg`)) {
+		return <Image {...props} src={`/images/country/${code}.svg`} unoptimized />;
+	}
+
+	return <Image {...props} src={`/images/country/${code}.png`} />;
+	// const {
+	// 	props: { srcSet: svg },
+	// } = getImageProps({
+	// 	...props,
+	// 	src: `/images/country/${code}.svg`,
+	// 	unoptimized: true,
+	// });
+	// const {
+	// 	props: { srcSet: png, ...rest },
+	// } = getImageProps({
+	// 	...props,
+	// 	src: `/images/country/${code}.png`,
+	// });
+
+	// return (
+	// 	<picture>
+	// 		<source type="image/svg" srcSet={svg} />
+	// 		<source type="image/png" srcSet={png} />
+
+	// 		<img {...rest} alt={`${code} Flag`} />
+	// 	</picture>
+	// );
 };
--- components/util/Image.tsx
+++ components/util/Image.tsx
@@ -1,5 +1,8 @@
-import { useState } from "react";
-import NextImage, { type ImageProps as NextImageProps } from "next/image";
+"use server";
+import NextImage, {
+	type ImageProps as NextImageProps,
+	getImageProps,
+} from "next/image";
 
 import type { CountryKey, GamesKey, SportKey } from "types/prisma";
 
@@ -19,45 +22,65 @@ type ImageProps = (CountryImageProps | GamesImageProps | SportsImageProps) &
 const useCountryImageSrc = (code: CountryKey) => {
 	if (code in sharedFlags) {
 		const sharedFlag = sharedFlags[code as keyof typeof sharedFlags];
-		return `/images/country/shared/${sharedFlag}`;
+		return [`/images/country/shared/${sharedFlag}`];
 	}
-	return `/images/country/${code}.svg`; // TODO: handle avif
+	return [`/images/country/${code}.svg`, `/images/country/${code}.avif`]; // TODO: handle avif
 };
 
 const useGamesImageSrc = (code: GamesKey) => {
-	return `/images/games/${code}/emblem.svg`; // TODO: handle avif
+	return [
+		`/images/games/${code}/emblem.svg`,
+		`/images/games/${code}/emblem.avif`,
+	]; // TODO: handle avif
 };
 
 const useSportsImageSrc = (code: SportKey, games?: GamesKey) => {
 	if (games) {
-		return `/images/games/${games}/sports/${code}.avif`; // TODO: handle fallback
+		return [`/images/games/${games}/sports/${code}.avif`]; // TODO: handle fallback
 	}
 
-	if (true) {
-		return `/images/sports/official/${code}.svg`;
-	}
-
-	return `/images/sports/${code}.svg`;
+	return [`/images/sports/official/${code}.svg`, `/images/sports/${code}.svg`];
 };
 
 export const Image = ({ dir, code, fallback, ...props }: ImageProps) => {
-	const src =
-		dir === "country"
-			? useCountryImageSrc(code)
-			: dir === "games"
-				? useGamesImageSrc(code)
-				: dir === "sports"
-					? useSportsImageSrc(
-							code,
-							(props as Omit<SportsImageProps, "dir" | "code">).games,
-						)
-					: "";
+	const src = (() => {
+		if (dir === "country") {
+			return useCountryImageSrc(code);
+		}
+		if (dir === "games") {
+			useGamesImageSrc(code);
+		}
+		if (dir === "sports") {
+			return useSportsImageSrc(
+				code,
+				(props as Omit<SportsImageProps, "dir" | "code">).games,
+			);
+		}
+		return [];
+	})();
+
+	return <BaseImage srcs={src} {...props} />;
+};
+
+interface BaseImageProps extends Omit<NextImageProps, "src"> {
+	srcs: string[];
+}
+
+const BaseImage = ({ srcs, ...props }: BaseImageProps) => {
+	const imagePropsList = srcs.map((src) => getImageProps({ src, ...props }));
+
+	const srcSets = imagePropsList.map((imgProps) => imgProps.props.srcSet);
+
+	const {
+		props: { srcSet, ...rest },
+	} = imagePropsList.at(-1)!;
 
 	return (
-		<NextImage
-			{...props}
-			src={src}
-			// onError={() => setImgSrc(fallback ?? src)}
-		/>
+		<picture>
+			{srcSets.map((srcSet, i) => (
+				<source key={i} srcSet={srcSet} />
+			))}
+			<img alt={props.alt} {...rest} />
+		</picture>
 	);
 };
