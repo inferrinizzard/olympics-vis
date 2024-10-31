aa10cdf115ada35b68377312277ba20366c413ee
inferrinizzard <inferrinizzard@gmail.com>
2024-10-31 08:19:56 -0700

    bootstrap SportsPictogramRow

---
+++ app/sports/_components/SportsPictogramRow/client.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+export interface SportsPictogramRowProps {}
+
+const SportsPictogramRow_Client = ({}: SportsPictogramRowProps) => {
+	return <></>;
+};
+
+export default SportsPictogramRow_Client;
---
+++ app/sports/_components/SportsPictogramRow/index.tsx
@@ -0,0 +1,9 @@
+import type { SportProps } from "types";
+
+import SportsPictogramRow_Client from "./client";
+
+const SportsPictogramRow = ({ sport }: SportProps) => {
+	return <SportsPictogramRow_Client />;
+};
+
+export default SportsPictogramRow;
