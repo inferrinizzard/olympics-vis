e582571fd12df15a6ad40ba8fb8ed859df54008d
inferrinizzard <inferrinizzard@gmail.com>
2024-11-03 18:04:02 -0800

    add basic fuzzy search in search modal

--- components/shell/Search/SearchModal.tsx
+++ components/shell/Search/SearchModal.tsx
@@ -1,6 +1,15 @@
-import type { Dispatch, SetStateAction } from "react";
+import {
+	useEffect,
+	useMemo,
+	useState,
+	type Dispatch,
+	type SetStateAction,
+} from "react";
 
-import { Modal } from "@mantine/core";
+import { Modal, TextInput } from "@mantine/core";
+import FuzzySearch from "fuzzy-search";
+
+import searchStrings from "./searchStrings.json";
 
 interface SearchModalProps {
 	isSearchOpen: boolean;
@@ -11,5 +20,31 @@ export const SearchModal = ({
 	isSearchOpen,
 	setIsSearchOpen,
 }: SearchModalProps) => {
-	return <Modal opened={isSearchOpen} onClose={() => setIsSearchOpen(false)} />;
+	const [searchText, setSearchText] = useState("");
+
+	const searcher = useMemo(
+		() => new FuzzySearch(searchStrings, ["code", "name"], { sort: true }),
+		[],
+	);
+
+	const searchResults = searchText.trim() ? searcher.search(searchText) : [];
+
+	useEffect(() => {
+		if (!isSearchOpen) {
+			setSearchText("");
+		}
+	}, [isSearchOpen]);
+
+	return (
+		<Modal
+			opened={isSearchOpen}
+			onClose={() => setIsSearchOpen(false)}
+			withCloseButton={false}
+		>
+			<TextInput onChange={(e) => setSearchText(e.target.value.trim())} />
+			{searchResults.map((str) => (
+				<div key={str.code}>{str.code}</div>
+			))}
+		</Modal>
+	);
 };
