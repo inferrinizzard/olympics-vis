f6108b3139e04b9fb1c9a5a531c58395f9de53f4
inferrinizzard <inferrinizzard@gmail.com>
2024-11-05 07:07:22 -0800

    update CountryOverview with vanilla

--- app/countries/_components/CountryOverview/client.tsx
+++ app/countries/_components/CountryOverview/client.tsx
@@ -1,4 +1,4 @@
-import { Box, Title } from "@mantine/core";
+import { Box, Group, Title } from "@mantine/core";
 
 import type { Country } from "@prisma/client";
 import type { GamesKey, SportKey } from "types/prisma";
@@ -13,6 +13,8 @@ import StatCard from "components/grid/StatCard";
 import Excerpt from "components/layouts/Excerpt";
 import { Image } from "components/util/Image";
 import { getGameName } from "lib/utils/getGameName";
+
+import * as classes from "./CountryOverview.css";
 
 interface CountryOverviewData {
 	firstGames: GamesKey;
@@ -34,30 +36,26 @@ const CountryOverview_Client = ({
 	overviewData: { firstGames, totalMedals, hostedGames, bestGames, bestSport },
 }: CountryOverviewProps) => {
 	return (
-		<GridCell
-			bg="blue"
-			h="100%"
-			// style={(theme) => ({ color: theme.colors.blue[2] })}
-		>
-			<Title order={1}>{`${country.name} (${country.code})`}</Title>
-			<Box mah="min(50%, 12rem)" style={{ position: "relative" }}>
+		<GridCell className={classes.GridContainer}>
+			<Title
+				order={1}
+				mt="xs"
+				mb="xs"
+				style={{ textAlign: "center" }}
+			>{`${country.name} (${country.code})`}</Title>
+			<Box h="10rem" w="100%" pos="relative">
 				<Image
 					dir="country"
 					code={country.code}
 					fill
-					style={{
-						objectFit: "scale-down",
-					}}
+					style={{ objectFit: "contain" }}
 					alt={`NOC Flag for ${country.code}`}
 				/>
 			</Box>
 			<Box m="0.5rem">
 				<Excerpt text={wikipediaExcerpt} />
 			</Box>
-			<Box
-				p="xs"
-				style={{ display: "flex", rowGap: "1rem", flexDirection: "column" }}
-			>
+			<Group className={classes.StatCardContainer}>
 				<StatCard
 					Icon={Calendar}
 					title={"First Games"}
@@ -75,7 +73,7 @@ const CountryOverview_Client = ({
 				{bestSport && (
 					<StatCard Icon={Run} title={"Best Sport"} text={bestSport} />
 				)}
-			</Box>
+			</Group>
 		</GridCell>
 	);
 };
