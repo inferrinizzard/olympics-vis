002b68ed8391bb9efef7abd9d00ec8cb49e9515d
inferrinizzard <inferrinizzard@gmail.com>
2024-11-07 07:43:58 -0800

    :recycle: country charts

--- app/countries/_components/CountryGamesMedalsChart/client.tsx
+++ app/countries/_components/CountryGamesMedalsChart/client.tsx
@@ -1,8 +1,8 @@
 "use client";
 
-import { type BarDatum, ResponsiveBar } from "@nivo/bar";
-
 import { Box, Title } from "@mantine/core";
+
+import { type BarDatum, ResponsiveBar } from "@nivo/bar";
 
 import GridCell from "components/grid/GridCell";
 
@@ -24,18 +24,31 @@ const CountryGamesMedalsChart_Client = ({
 		);
 	}
 
+	// TODO: limit initial view, expand to modal to see all
+	// TODO: update games X label based on # of bars (only years + O/P/Y) if many
+	// TODO: custom tooltip
+	// TODO: controls for season, edition, year ?
+	// TODO: typing
+
 	return (
 		<GridCell>
 			<Title order={2}>{"Medals per Game"}</Title>
 			<Box h="30vh" w="100%">
 				<ResponsiveBar
+					// Data
 					data={data}
 					keys={keys}
 					indexBy="games"
-					margin={{ top: 20, bottom: 50, left: 30 }}
+					// Scale
 					valueScale={{ type: "linear" }}
 					indexScale={{ type: "band" }}
+					// Labelling
+					enableLabel={false}
+					enableTotals
+					// Style
+					margin={{ top: 20, bottom: 50, left: 30 }}
 					colors={{ scheme: "nivo" }}
+					// Axes
 					axisBottom={{
 						tickSize: 5,
 						tickPadding: 5,
--- app/countries/_components/CountrySportsMedalsChart/client.tsx
+++ app/countries/_components/CountrySportsMedalsChart/client.tsx
@@ -24,18 +24,30 @@ const CountrySportsMedalsChart_Client = ({
 		);
 	}
 
+	// TODO: limit initial view, expand to modal to see all
+	// TODO: custom tooltip
+	// TODO: controls for season
+	// TODO: typing
+
 	return (
 		<GridCell>
 			<Title order={2}>{"Medals per Sport"}</Title>
 			<Box w="100%" h="30vh">
 				<ResponsiveBar
-					data={data}
+					// Data
+					data={data.filter((datum) => keys.some((key) => datum[key]))}
 					keys={keys}
 					indexBy="sport"
-					margin={{ top: 20, bottom: 50, left: 30 }}
+					// Scale
 					valueScale={{ type: "linear" }}
 					indexScale={{ type: "band" }}
+					// Labelling
+					enableLabel={false}
+					enableTotals
+					// Style
+					margin={{ top: 20, bottom: 50, left: 30 }}
 					colors={{ scheme: "nivo" }}
+					// Axes
 					axisBottom={{
 						tickSize: 5,
 						tickPadding: 5,
