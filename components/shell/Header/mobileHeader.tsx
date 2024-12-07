42d55f391f2919dfbb461d419c0de0fe7a886505
inferrinizzard <inferrinizzard@gmail.com>
2024-12-07 09:30:31 -0800

    flesh out dropdown section

--- components/shell/Header/mobileHeader.tsx
+++ components/shell/Header/mobileHeader.tsx
@@ -1,10 +1,11 @@
 import Link from "next/link";
 
-import { Group, ActionIcon, Title, Button } from "@mantine/core";
-import Home from "tabler-icons-react/dist/icons/home";
+import { Group, ActionIcon, Title } from "@mantine/core";
+import Menu from "tabler-icons-react/dist/icons/menu-2";
 
-import ColorSchemeToggle from "components/controls/ColorSchemeToggle";
+import { vars } from "styles/theme";
 
+import ColorSchemeToggle from "../../controls/ColorSchemeToggle";
 import { Search } from "../../controls/Search";
 
 import type { SharedHeaderProps } from "./types";
@@ -22,13 +23,54 @@ export const MobileHeader = ({
 			</Group>
 
 			<Group>
-				'mobile'
-				{/* <Button /> */}
-				{/* <Search />
-				<ColorSchemeToggle /> */}
+				<ActionIcon
+					size="lg"
+					variant="filled"
+					bg={vars.colors.primary}
+					onClick={() => {}}
+				>
+					<Menu />
+				</ActionIcon>
 			</Group>
 
-			<Group style={{ position: "absolute" }}>{/*  */}</Group>
+			<Group
+				style={{
+					borderTop: `solid 1px ${vars.colors.primary}`,
+					backgroundColor: vars.colors.body,
+					position: "absolute",
+					top: "100%",
+					insetInline: 0,
+					padding: vars.spacing.xs,
+				}}
+			>
+				<Group>
+					<Search />
+					<ColorSchemeToggle />
+				</Group>
+				<Group style={{ width: "100%", justifyContent: "space-evenly" }}>
+					<Link
+						passHref
+						href="/games"
+						className={activeHeaderLinkClassFn("games")}
+					>
+						<Title order={3}>{"Games"}</Title>
+					</Link>
+					<Link
+						passHref
+						href="/countries"
+						className={activeHeaderLinkClassFn("countries")}
+					>
+						<Title order={3}>{"Countries"}</Title>
+					</Link>
+					<Link
+						passHref
+						href="/sports"
+						className={activeHeaderLinkClassFn("sports")}
+					>
+						<Title order={3}>{"Sports"}</Title>
+					</Link>
+				</Group>
+			</Group>
 		</>
 	);
 };
