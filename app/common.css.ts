daf6e8c955d5fbd6f275997e70e6c6f5490ab9d4
inferrinizzard <inferrinizzard@gmail.com>
2024-12-07 14:40:44 -0800

    vertical align all pages with shared class

--- app/common.css.ts
+++ app/common.css.ts
@@ -2,7 +2,7 @@ import { style } from "@vanilla-extract/css";
 
 import { vars } from "styles/theme";
 
-export const GamesPageGridCol = style({
+export const GridCol = style({
 	"@media": {
 		[`screen and (max-width: ${vars.breakpoints.sm})`]: {
 			width: "100%",
