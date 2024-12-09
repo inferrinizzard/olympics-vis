3d335164906fc0acec317cba68887f6b047a0ebb
inferrinizzard <inferrinizzard@gmail.com>
2024-12-09 00:22:04 -0800

    add script to fetch imageMap at build time

--- components/util/Image.tsx
+++ components/util/Image.tsx
@@ -1,13 +1,11 @@
 "use server";
 
-import { existsSync } from "node:fs";
-
 import NextImage, { type ImageProps as NextImageProps } from "next/image";
 
 import type { CountryKey, GamesKey, Sport, SportKey } from "types/prisma";
 
-import sharedFlags from "public/images/country/shared/sharedFlags.json";
-import parentDisciplineMap from "public/images/sports/parentDisciplineMap.json";
+import imageMap from "./imageMap.json";
+import { buildImageMapKey } from "./getImageSrc";
 
 export type CountryImageProps = { dir: "country"; code: CountryKey };
 export type GamesImageProps = { dir: "games"; code: GamesKey };
@@ -27,125 +25,14 @@ export type ImageProps = (
 ) &
 	FallbackImageProps;
 
-const imageMap: Record<string, string> = {};
-
-// TODO: add final fallbacks and remove png
-export const getCountryImageSrc = async (code: CountryKey) => {
-	const mapKey = `country:${code}`;
-
-	if (mapKey in imageMap) {
-		return imageMap[mapKey];
-	}
-
-	if (code in sharedFlags) {
-		const sharedFlag = sharedFlags[code as keyof typeof sharedFlags];
-		return `/images/country/shared/${sharedFlag}`;
-	}
-
-	const match = ["svg", "avif", "png"]
-		.map((ext) => `/images/country/${code}.${ext}`)
-		.find((path) => existsSync(`public/${path}`));
-
-	if (match) {
-		imageMap[mapKey] = match;
-	}
-
-	return match;
-};
-
-export const getGamesImageSrc = async (code: GamesKey) => {
-	const mapKey = `games:${code}`;
-
-	if (mapKey in imageMap) {
-		return imageMap[mapKey];
-	}
-
-	const match = ["svg", "avif", "png", "jpg"]
-		.map((ext) => `/images/games/${code}/emblem.${ext}`)
-		.find((path) => existsSync(`public/${path}`));
-
-	if (match) {
-		imageMap[mapKey] = match;
-	}
-
-	return match;
-};
-
-export const getSportsImageSrc = async (
-	code: SportKey,
-	parent?: Sport["parent"],
-	games?: GamesKey,
-): Promise<string | undefined> => {
-	const mapKey = `sport:${[code, games].join("+")}`;
-
-	if (mapKey in imageMap) {
-		return imageMap[mapKey];
-	}
-
-	const paths = [];
-
-	if (games) {
-		// paths.push(`/images/games/${games}/sports/${code}.avif`);
-		paths.push(`/images/games/${games}/sports/${code}.png`);
-	}
-
-	paths.push(
-		`/images/sports/official/${code}.svg`,
-		`/images/sports/official/${code}.avif`,
-		`/images/sports/${code}.svg`,
-		`/images/sports/${code}.avif`,
-	);
-
-	if (parent) {
-		const parentPath = await getSportsImageSrc(parent, undefined, games);
-		if (parentPath) {
-			paths.push(parentPath);
-		}
-	}
-
-	if (code in parentDisciplineMap) {
-		const disciplines =
-			parentDisciplineMap[code as keyof typeof parentDisciplineMap];
-		const validPaths = await Promise.all(
-			disciplines.flatMap((d) => getSportsImageSrc(d) ?? []),
-		);
-		paths.push(...validPaths);
-	}
-
-	const match = paths.find((path) => existsSync(`public/${path}`));
-
-	if (match) {
-		imageMap[mapKey] = match;
-	}
-
-	return match;
-};
-
 export const Image = async ({ dir, code, ...props }: ImageProps) => {
-	const srcGetter = async () => {
-		if (dir === "country") {
-			return (await getCountryImageSrc(code)) ?? "";
-		}
-		if (dir === "games") {
-			return (await getGamesImageSrc(code)) ?? "";
-		}
-		if (dir === "sports") {
-			return (
-				(await getSportsImageSrc(
-					code,
-					(props as Omit<SportsImageProps, "dir" | "code">).parent,
-					(props as Omit<SportsImageProps, "dir" | "code">).games,
-				)) ?? ""
-			);
-		}
-		return "";
-	};
+	const mapKey = buildImageMapKey(dir, code, [
+		"games" in props ? props.games : undefined,
+	]);
+	const src = imageMap[mapKey];
 
-	const src =
-		(await srcGetter()) ||
-		(code.startsWith("P-")
-			? "/images/country/shared/Paralympic_flag.svg"
-			: "/images/country/shared/Olympic_flag.svg");
+	//  "/images/country/shared/Paralympic_flag.svg"
+	//  "/images/country/shared/Olympic_flag.svg");
 
 	return <NextImage {...props} src={src} unoptimized={src.endsWith(".svg")} />;
 };
---
+++ components/util/getImageSrc.ts
@@ -0,0 +1,72 @@
+import { existsSync } from "node:fs";
+
+import type { CountryKey, GamesKey, Sport, SportKey } from "types/prisma";
+
+import sharedFlags from "public/images/country/shared/sharedFlags.json";
+import parentDisciplineMap from "public/images/sports/parentDisciplineMap.json";
+
+export const buildImageMapKey = (
+	dir: string,
+	code: string,
+	extra: (string | undefined)[] = [],
+) => `${dir}:${[code, ...extra.map((x) => x)].join("+")}`;
+
+// TODO: add final fallbacks and remove png
+export const getCountryImageSrc = (code: CountryKey) => {
+	if (code in sharedFlags) {
+		const sharedFlag = sharedFlags[code as keyof typeof sharedFlags];
+		return `/images/country/shared/${sharedFlag}`;
+	}
+
+	const match = ["svg", "avif", "png"]
+		.map((ext) => `/images/country/${code}.${ext}`)
+		.find((path) => existsSync(`public/${path}`));
+
+	return match;
+};
+
+export const getGamesImageSrc = (code: GamesKey) => {
+	const match = ["svg", "avif", "png", "jpg"]
+		.map((ext) => `/images/games/${code}/emblem.${ext}`)
+		.find((path) => existsSync(`public/${path}`));
+
+	return match;
+};
+
+export const getSportsImageSrc = (
+	code: SportKey,
+	parent?: Sport["parent"],
+	games?: GamesKey,
+): string | undefined => {
+	const paths = [];
+
+	if (games) {
+		// paths.push(`/images/games/${games}/sports/${code}.avif`);
+		paths.push(`/images/games/${games}/sports/${code}.png`);
+	}
+
+	paths.push(
+		`/images/sports/official/${code}.svg`,
+		`/images/sports/official/${code}.avif`,
+		`/images/sports/${code}.svg`,
+		`/images/sports/${code}.avif`,
+	);
+
+	if (parent) {
+		const parentPath = getSportsImageSrc(parent, undefined, games);
+		if (parentPath) {
+			paths.push(parentPath);
+		}
+	}
+
+	if (code in parentDisciplineMap) {
+		const disciplines =
+			parentDisciplineMap[code as keyof typeof parentDisciplineMap];
+		const validPaths = disciplines.flatMap((d) => getSportsImageSrc(d) ?? []);
+		paths.push(...validPaths);
+	}
+
+	const match = paths.find((path) => existsSync(`public/${path}`));
+
+	return match;
+};
