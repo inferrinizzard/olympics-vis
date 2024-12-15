af25137b449b9dc265279cb59eb274a1246662eb
inferrinizzard <inferrinizzard@gmail.com>
2024-12-15 11:36:09 -0800

    share data function for sports page

--- app/sports/layout.tsx
+++ app/sports/layout.tsx
@@ -1,16 +1,16 @@
 import type { PropsWithChildren } from "react";
 import { MantineProvider } from "@mantine/core";
 
-import { getAllSports } from "lib/db";
 import PageNavButtons from "components/controls/PageNavButtons";
 
+import { getSportsForPage } from "./_data";
 import { sportsTheme } from "./_theme";
 
 export default async function SportsLayout({ children }: PropsWithChildren) {
-	const sports = await getAllSports({
-		orderBy: [{ season: "asc" }, { status: "asc" }, { code: "asc" }],
-		where: { category: "discipline" },
-	});
+	const sportsLists = await getSportsForPage(true);
+	const sports = Object.values(sportsLists).reduce((acc, cur) =>
+		acc.concat(cur),
+	);
 
 	return (
 		<MantineProvider theme={sportsTheme}>
